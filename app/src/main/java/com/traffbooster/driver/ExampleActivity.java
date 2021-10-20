8ec4d1d496ae91cd9bde64fc1070a8f109d4c200
shtefunik <shtefunik@mdcm.co>
2021-10-20 10:13:19 +0300

    f

--- app/src/main/java/com/traffbooster/driver/ExampleActivity.java
+++ app/src/main/java/com/traffbooster/driver/ExampleActivity.java
@@ -10,7 +10,5 @@ public class ExampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example);
-
-        String s = "";
     }
 }
